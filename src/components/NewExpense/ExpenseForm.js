f5b7f9fcc74dac766de50daa9a999670c2aad385
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-05-07 13:39:37 +0100

    clean up

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,87 +1,68 @@
-import React, { useState } from 'react'
-import './ExpenseForm.css'
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 function ExpenseForm(props) {
-    const [title, setTitle] = useState('')
-    const [amount, setAmount] = useState('')
-    const [date, setDate] = useState('')
-    // const [userInput, setUserInput] = useState({
-    //     title: '',
-    //     amount: '',
-    //     date: ''
-    // })
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
 
-    const titleChangeHandler = (event) => {
-        setTitle(event.target.value)
-        // setUserInput({
-        //     ...userInput,
-        //     title: event.target.value,
-        // })
-    }
-    const amountChangeHandler = (event) => {
-        setAmount(event.target.value)
-        // setUserInput({
-        //     ...userInput,
-        //     amount: event.target.value,
-        // })
-    }
-    const dateChangeHandler = (event) => {
-        setDate(event.target.value)
-        // setUserInput({
-        //     ...userInput,
-        //     date: event.target.value,
-        // })
-    }
-    const submitHandler = (event) => {
-        event.preventDefault();
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-        const expenseData = {
-            title, amount: +amount, date: new Date(date)
-        }
-        props.onSaveExpenseData(expenseData);
-        setTitle('');
-        setAmount('');
-        setDate('');
-        props.handleToggle(event.target.value)
-    }
-    return (
-        <form onSubmit = { submitHandler }>
-            <div className = "new-expense__controls">
-                <div className = "new-expense__control">
-                    <label>Title</label>
-                    <input 
-                    type = "text" 
-                    onChange = { titleChangeHandler } 
-                    value = { title }
-                    />
-                </div>
-                <div className = "new-expense__control">
-                    <label>Amount</label>
-                    <input 
-                    type = "number" 
-                    min="0.01" 
-                    step="0.01" 
-                    onChange = { amountChangeHandler } 
-                    value = { amount }
-                    />
-                </div>
-                <div className = "new-expense__control">
-                    <label>Date</label>
-                    <input 
-                    type = "date" 
-                    min="2019-01-01" 
-                    max="2022-12-31" 
-                    onChange = { dateChangeHandler } 
-                    value = { date }
-                    />
-                </div>
-            </div>
-            <div className = "new-expense__actions ">
-                <button onClick = {props.onRemoveForm}>Cancel</button>
-                <button type = "submit">Add Expense</button>
-            </div>
-        </form>
-    )
+    const expenseData = {
+      title,
+      amount: +amount,
+      date: new Date(date),
+    };
+    props.onSaveExpenseData(expenseData);
+    setTitle("");
+    setAmount("");
+    setDate("");
+    props.handleToggle(event.target.value);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} value={title} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+            value={amount}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+            value={date}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions ">
+        <button onClick={props.onRemoveForm}>Cancel</button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 }
 
-export default ExpenseForm
+export default ExpenseForm;
